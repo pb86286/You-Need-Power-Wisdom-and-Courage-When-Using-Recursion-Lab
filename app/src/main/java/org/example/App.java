3a04ac7be60763c70d6a3d961d603c0da59b5576
pb86286 <pb86286@mail.harpercollege.edu>
2025-07-06 22:44:55 -0500

    Code

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,16 +4,16 @@ public class App {
     public static void main(String[] args) {
         TriangleNumberCalculator calculator = new TriangleNumberCalculator();
 
-        System.out.println(calculator.value(1)); // 1
-        System.out.println(calculator.value(2)); // 3
-        System.out.println(calculator.value(4)); // 10
+        System.out.println(calculator.value(1)); 
+        System.out.println(calculator.value(2)); 
+        System.out.println(calculator.value(4)); 
 
-        System.out.println(calculator.add(1, 1)); // 2
-        System.out.println(calculator.add(2, 3)); // 9
-        System.out.println(calculator.add(4, 2)); // 13
+        System.out.println(calculator.add(1, 1));
+        System.out.println(calculator.add(2, 3)); 
+        System.out.println(calculator.add(4, 2)); 
 
-        System.out.println(calculator.subtract(1, 1)); // 0
-        System.out.println(calculator.subtract(2, 3)); // -3
-        System.out.println(calculator.subtract(4, 2)); // 7
+        System.out.println(calculator.subtract(1, 1)); 
+        System.out.println(calculator.subtract(2, 3)); 
+        System.out.println(calculator.subtract(4, 2)); 
     }
 }
